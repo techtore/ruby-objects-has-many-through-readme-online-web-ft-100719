6df664879c59cac1db699f5ff0c506a6db3b7934
Victoria Hale <vic.hale74@gmail.com>
2019-10-25 14:43:57 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,9 +13,9 @@ class Customer
     @@all 
   end 
   
-  def new_meal(@waiter,total, tip=0)
-   
-    end
+   def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
   end
   
   def waiters
