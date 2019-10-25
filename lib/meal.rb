1e40963c376d992938d1adc2a303fdf096745614
Victoria Hale <vic.hale74@gmail.com>
2019-10-25 14:22:20 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,6 +1,8 @@
 class Meal
+  
+  attr_accessor :water, :customer, :total, :tip
 
-  def initialize(wauter, customer, total, tip)
+  def initialize(water, customer, total, tip)
     @waiter = waiter
     @customer = customer
     @total = total 
