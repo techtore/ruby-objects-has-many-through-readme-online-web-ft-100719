f70bdc42cae8a28cb02d922d04ddabddbb888176
Victoria Hale <vic.hale74@gmail.com>
2019-10-25 14:48:23 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -17,6 +17,12 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end
   
+  def meals
+    Meal.all.select do |meal|
+    meal.customer == self
+    end
+  end
+  
   def waiters
     meals.map do |meal|
       meal.waiter
